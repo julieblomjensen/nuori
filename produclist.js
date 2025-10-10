a4736ddd9062b1a4c72cb0a490665f9e2830ea25
Manar <mael0009@stud.kea.dk>
2025-10-10 12:21:03 +0200

    gjort listeview dynamisk

---
+++ produclist.js
@@ -0,0 +1,26 @@
+const ProductListContainer = document.querySelector(".grid-products")
+
+fetch(`https://dummyjson.com/products/category/beauty`)
+.then((response) => response.json())
+.then(data => {
+    allData = currentDataSet = data;
+    showProducts(allData);
+})
+
+
+    
+function showProducts(allData){
+    ProductListContainer.innerHTML = "";
+    allData.products.forEach(element=> {
+        console.log(element);
+        ProductListContainer.innerHTML += ` <div class="product-card">
+          <p class="brand-name">${element.brand}</p>
+            <a href="product.html"><img src="${element.thumbnail}" alt="produkt" /></a>
+          <p>${element.title}</p>
+          <p>${element.price},-</p>
+          <img class="basket-dark" src="assets/Basket dark.png" alt="basket" />
+        </div>`;
+    })}
+
+    
+  
